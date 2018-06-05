1e30ee979999dabd0b03452c7f27a9d87d36c20d
Pengsai <774506988@qq.com>
2018-06-05 10:04:55 +0800

    修改jdbc配置

--- jt-manage/jt-manage-controller/src/main/java/com/jt/manage/controller/IndexController.java
+++ jt-manage/jt-manage-controller/src/main/java/com/jt/manage/controller/IndexController.java
@@ -1,6 +1,5 @@
 package com.jt.manage.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +14,7 @@ public class IndexController {
 
     @RequestMapping(value = "/index")
     public String index2(){
-        System.out.println("sss");
+        System.out.println("ssss");
         return "index";
     }
 }
